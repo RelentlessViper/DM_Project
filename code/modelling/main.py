89492bcdf8fabbbab9df40024f7809c5c2f4a305
Israel Adewuyi <isistickz@gmail.com>
2025-04-10 18:13:37 +0300

    implemented hyper param search for HDBSCAN

--- code/modelling/main.py
+++ code/modelling/main.py
@@ -1,4 +1,6 @@
 import umap
+import yaml
+import json
 import torch
 import argparse
 import numpy as np
@@ -9,100 +11,48 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
 from transformers import BertTokenizer, BertModel
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-
-def load_data(file_path, sample_size=None):
-    """Load the dataset. To load a small sample fo the dataset, set the sample size"""
-    df = pd.read_csv(file_path)
-    df['text'] = df['title'] + " " + df['abstract']
-    
-    if sample_size:
-        df = df.sample(sample_size, random_state=42)
+from clustering import cluster_embeddings
+from embedding_generator import generate_or_load_embeddings
+from utils.loader_utils import load_data, load_model_tokenizer, load_best_hdbscan_config
+from utils.cluster_utils import visualize_clusters
         
-    return df
+def main(config_path: str ="config.yaml"):
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
 
-
-def get_embeddings(texts, batch_size=32):
-    """Batch the input (text + abstract) and generate the embeddings"""
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model = BertModel.from_pretrained('bert-base-uncased').to(device)
-    model.eval() 
-
-    embeddings = []
-    for i in tqdm(range(0, len(texts), batch_size), desc="Generating Embeddings"):
-        batch_texts = texts[i:i + batch_size]
-        inputs = tokenizer(batch_texts, return_tensors='pt', padding=True, 
-                          truncation=True, max_length=512).to(device)
-        with torch.no_grad():
-            outputs = model(**inputs)
-            batch_embeddings = outputs.last_hidden_state[:, 0, :].cpu().numpy()
-        embeddings.append(batch_embeddings)
+    hdbscan_config = load_best_hdbscan_config("")     
+    config = {
+        **config,  # Base config
+        **{k: v for k, v in hdbscan_config.items() 
+           if k not in ['silhouette', 'dbcv', 'labels']}
+    }
     
-    return np.vstack(embeddings)
-
-
-def cluster_embeddings(embeddings, n_clusters: int):
-    """Cluster embeddings"""
-    kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
-    cluster_labels = kmeans.fit_predict(embeddings)
-    
-    silhouette_avg = silhouette_score(embeddings, cluster_labels)
-    print(f"Silhouette Score: {silhouette_avg:.3f}")
-    return cluster_labels, kmeans
-
-def visualize_clusters(embeddings, cluster_labels):
-    reducer = umap.UMAP(n_components=2, random_state=42)
-    embeddings_2d = reducer.fit_transform(embeddings)
-
-    df_plot = pd.DataFrame({
-        "x": embeddings_2d[:, 0],
-        "y": embeddings_2d[:, 1],
-        "cluster": cluster_labels
-    })
-
-    fig = px.scatter(
-        df_plot,
-        x="x",
-        y="y",
-        color="cluster",
-        title="Interactive Cluster Visualization",
-        labels={"cluster": "Cluster"},
-        hover_data=["cluster"],  
-        color_continuous_scale=px.colors.sequential.Plasma 
-    )
-
-    fig.write_html("interactive_plot.html")
-    fig.show()
-
-def main(file_path, sample_size: int, n_clusters: int):
+    print("Loaded optimized HDBSCAN configuration")
+        
     print("Loading data...")
-    df = load_data(file_path, sample_size=sample_size)
-    
-    print("Generating BERT embeddings...")
-    embeddings = get_embeddings(df['text'].tolist())
-    
-    print("Clustering...")
-    cluster_labels, kmeans = cluster_embeddings(embeddings, n_clusters=n_clusters)
-    
-    print("Visualizing...")
-    visualize_clusters(embeddings, cluster_labels)
-    
-    df.to_csv("clustered_data.csv", index=False)
-    print("Results saved to 'clustered_data.csv'")
+    df = load_data(config["file_path"], sample_size=config["sample_size"])
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Read args for clustering")
+    print("Loading model and tokenizer")
+    model, tokenizer = load_model_tokenizer(config["model_name"], device=config["device"])
 
-    parser.add_argument("--file_path", type=str, default="../ignore_folder/data/docs_df.csv",
-                        help="Path to .csv")
-    parser.add_argument("--sample_size", type=int, default=None,
-                        help="Sample size for experimentations")
-    parser.add_argument("--num_clusters", type=int, default=20,
-                        help="Number of embedding clusters")
-    return parser.parse_args()
+    assert model is not None
+    assert tokenizer is not None
+
+    embeddings = generate_or_load_embeddings(model, tokenizer, df['text'].tolist(), config)
+    embeddings = StandardScaler().fit_transform(embeddings)
+    
+    cluster_labels, cluster_model = cluster_embeddings(embeddings, config)
+
+    print(cluster_labels)
+
+    print("Visualizing clusters...")
+    visualize_clusters(embeddings, cluster_labels, config)
+
+    
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args.file_path, args.sample_size, args.num_clusters)
+    # args = parse_args()
+    main()
